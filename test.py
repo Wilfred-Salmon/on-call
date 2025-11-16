e25b1021896f3be761f21e91b401cee25f9f48a5
Wilfred-Salmon <Gwilfreds@gmail.com>
2025-11-16 10:49:25 +0000

    rename scratchpad file and remove from git

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.rotas import load_db
-from src.rotas import get_rota_between
-from datetime import date
-
-load_db(
-    ["users", "rotas", "change_dates", "rota_snapshots", "overrides"],
-    "./data"
-)
-print(get_rota_between(date(2025,1,1), date(2025,10,1), 0))
