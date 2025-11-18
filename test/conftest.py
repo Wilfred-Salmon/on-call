6f46fcd894292600f236a82fa7df357912015b7c
Wilfred-Salmon <Gwilfreds@gmail.com>
2025-11-18 23:19:35 +0000

    Set up fresh db fixture

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,21 @@
 import pytest
+import shutil
 from src.db import build_db, DEFAULT_TABLE_LIST
+
+TEST_DB_DIRECTORY = "./test/test_data"
 
 @pytest.fixture(scope="session")
 def shared_db():
-    db = build_db(DEFAULT_TABLE_LIST, "./test/test_data")
+    db = build_db(DEFAULT_TABLE_LIST, TEST_DB_DIRECTORY)
 
     yield db
 
     db.close()
 
-def fresh_db():
-    db = build_db(DEFAULT_TABLE_LIST, "./test/test_data")
+@pytest.fixture()
+def fresh_db(tmp_path):
+    shutil.copytree(TEST_DB_DIRECTORY, tmp_path, dirs_exist_ok = True)
+    db = build_db(DEFAULT_TABLE_LIST, tmp_path)
 
     yield db
 
--- test/test_db.py
+++ test/test_db.py
@@ -5,3 +5,8 @@ def test_build_db(shared_db: db.DuckDBPyConnection):
     tables = {item[0] for item in shared_db.sql("SHOW TABLES").fetchall()}
 
     assert tables == set(DEFAULT_TABLE_LIST)
+
+def test_fresh_db(fresh_db: db.DuckDBPyConnection):
+    tables = {item[0] for item in fresh_db.sql("SHOW TABLES").fetchall()}
+
+    assert tables == set(DEFAULT_TABLE_LIST)
