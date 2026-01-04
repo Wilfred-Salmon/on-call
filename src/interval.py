915e176c6170bbd9397dfbdb67227284cfc54ccb
Wilfred-Salmon <Gwilfreds@gmail.com>
2026-01-04 10:26:19 +0000

    change interval to a dataclass

--- src/interval.py
+++ src/interval.py
@@ -1,12 +1,10 @@
 from datetime import date
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
 class Interval:
     start: date
     end: date
-
-    def __init__(self, start: date, end: date) -> None:
-        self.start = start
-        self.end = end
 
     def __repr__(self) -> str:
         return(f"{str(self.start)} -> {str(self.end)}")
