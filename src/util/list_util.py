d68959e1d7b64e380c6d00d75941982ebbcffb6a
Wilfred-Salmon <Gwilfreds@gmail.com>
2026-04-04 12:16:44 +0100

    Change pre-commit config to yaml

--- src/util/list_util.py
+++ src/util/list_util.py
@@ -3,10 +3,6 @@ from typing import TypeVar, Generator
 T = TypeVar("T")
 
 
-def get_at_index_with_wrap(list: list[T], index: int) -> T:
-    return list[index % len(list)]
-
-
 def get_cyclical_list_iterator(list: list[T]) -> Generator[T]:
     size = len(list)
     index = 0
