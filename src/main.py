57414a13676356380ce81dad896a86732ed5989e
Wilfred-Salmon <Gwilfreds@gmail.com>
2026-05-31 11:14:40 +0100

    Add endpoint

--- src/main.py
+++ src/main.py
@@ -2,16 +2,15 @@ from collections.abc import AsyncGenerator
 
 from fastapi import FastAPI, Depends, HTTPException
 from contextlib import asynccontextmanager
-from src.rota.rotas import get_rota_between, Rota_Assignment
+from src.rota.rotas import add_user_to_rota_on_date, get_rota_between, Rota_Assignment
 from src.db.db import DB, DEFAULT_TABLE_SCHEMA
-from datetime import date
-from duckdb import DuckDBPyConnection
+from datetime import date, datetime
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
     db = DB(DEFAULT_TABLE_SCHEMA, "./data")
-    app.state.db = db.get_db()
+    app.state.db = db
     yield
     db.close()
 
@@ -19,7 +18,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
 app = FastAPI(lifespan=lifespan)
 
 
-def get_db() -> DuckDBPyConnection:
+def get_db() -> DB:
     return app.state.db
 
 
@@ -33,9 +32,22 @@ def get_rota_between_(
     start_date: date,
     end_date: date,
     rota_id: int,
-    db: DuckDBPyConnection = Depends(get_db),
+    db: DB = Depends(get_db),
 ) -> list[Rota_Assignment]:
     try:
-        return get_rota_between(start_date, end_date, rota_id, db)
+        return get_rota_between(start_date, end_date, rota_id, db.get_db())
     except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/rota")
+def add_user_to_rota_(
+    user_id: int,
+    rota_id: int,
+    date: datetime,
+    db: DB = Depends(get_db),
+) -> None:
+    try:
+        add_user_to_rota_on_date(user_id, rota_id, date, db)
+    except ValueError as e:
+        raise HTTPException(status_code=500, detail=str(e))
